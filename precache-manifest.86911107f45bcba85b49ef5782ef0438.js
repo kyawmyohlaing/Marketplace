fc5e42ea7eb750175204041071dd82575e283ae7
Kyaw Myo Hlaing <kyawthanthat@gmail.com>
2020-04-03 21:18:34 +0630

    Updates

---
+++ precache-manifest.86911107f45bcba85b49ef5782ef0438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a904b10ebc4f1608859f",
+    "url": "/Marketplace/static/css/main.ee236c0f.chunk.css"
+  },
+  {
+    "revision": "a904b10ebc4f1608859f",
+    "url": "/Marketplace/static/js/main.a904b10e.chunk.js"
+  },
+  {
+    "revision": "1c5f1d5a7b28bfdb752d",
+    "url": "/Marketplace/static/css/1.ce60917e.chunk.css"
+  },
+  {
+    "revision": "1c5f1d5a7b28bfdb752d",
+    "url": "/Marketplace/static/js/1.1c5f1d5a.chunk.js"
+  },
+  {
+    "revision": "47dc418c37a1d5530b0c",
+    "url": "/Marketplace/static/js/runtime~main.47dc418c.js"
+  },
+  {
+    "revision": "299c1505e0d01a99a558f226401f0853",
+    "url": "/Marketplace/index.html"
+  }
+];
